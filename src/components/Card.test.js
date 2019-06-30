54afdc5bedf2c176f459c8797aee7a9ed361acaa
Renato Reis <email@renatoamreis.com>
2019-06-30 11:13:08 +0200

    Adding Prettier

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,14 +1,13 @@
-import { shallow } from 'enzyme';
-import React from 'react'
-import Card from './Card';
+import { shallow } from "enzyme";
+import React from "react";
+import Card from "./Card";
 
 // shallow is the one we will use 90% of the time
 
-// mount 
+// mount
 
 // render - It's use to render react components to a static html
 
-
-it('expect to render Card Component', () => {
-    expect(shallow(<Card />)).toMatchSnapshot()
-})
+it("expect to render Card Component", () => {
+  expect(shallow(<Card />)).toMatchSnapshot();
+});
--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ErrorBoundry extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            hasError: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
 
-    //This basically it will be trigged if there is an error in our app to load a component
-    componentDidCatch(error, info) {
-        this.setState({ hasError: true })
-    }
+  //This basically it will be trigged if there is an error in our app to load a component
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
 
-    render() {
-        if (this.state.hasError) { //This means if there is error === true
-            return <h1>Ooooops. That is not good!</h1>
-        }
-        return this.props.children
+  render() {
+    if (this.state.hasError) {
+      //This means if there is error === true
+      return <h1>Ooooops. That is not good!</h1>;
     }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundry;
